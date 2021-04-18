cf564633517be6b5add366688f275afd3e53fd42
littlecheng <599013081@qq.com>
2021-04-18 22:34:35 +0800

    添加gateway模块，控制访问请求

--- springcloud-eureka-server-001/src/main/java/com/example/springcloud/EurekaServerApplication.java
+++ springcloud-eureka-server-001/src/main/java/com/example/springcloud/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcloud.eurekaserver;
+package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springcloud-eureka-server-001/src/test/java/com/example/springcloud/SpringcloudEurekaServer7001ApplicationTests.java
+++ springcloud-eureka-server-001/src/test/java/com/example/springcloud/SpringcloudEurekaServer7001ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springcloud.eurekaserver;
+package com.example.springcloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
